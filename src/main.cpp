2065bbaddc2cd31b3dc0002dfeea65aa00c771ed
ahlbany <ahlbany9@gmail.com>
2026-03-25 15:41:27 +0100

    V1.0.1: added loop, exit command, and input validation

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,35 @@
 #include "../include/calculator.h"
 
 int main() {
-    double a, b;
-    char op;
-    std::string error;
+    while(true) {
+        double a, b;
+        char op;
+        std::string error;
 
-    std::cout << "Enter expression (e.g., 2, 3): ";
-    std::cin >> a >> op >> b;
-    double result = calculate(a, b, op, error);
-    if(error.empty()) {
-        std::cout << "Result: " << result << std::endl;
-    } else {
-        std::cout << error << std::endl;
+        std::cout << "\nEnter expression (e.g., 2, 3) or 'q' to quit: ";
+
+        // check for Quit command first 
+        if (std::cin.peek() == 'q') {
+            break;
+        }
+
+        if (!(std::cin >> a >> op >> b )) {
+            std::cout << "invalid input\n";
+
+            std::cin.clear(); // clear error state
+            std::cin.ignore(1000, '\n'); // discard invalid input
+            continue;
+        }
+
+        double result = calculate(a, b, op, error);
+        if(error.empty()) {
+            std::cout << "Result: " << result << std::endl;
+        } else {
+            std::cout << error << std::endl;
+        }
     }
+    std::cout << "Calculator exited.\n";
     return 0;
 }
+
+//   std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // discard invalid input
